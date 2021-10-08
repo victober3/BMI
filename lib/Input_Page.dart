1bad73fadda20362f343d8bf6ab4a4e0bfd12f07
victober3 <vicobabolodje@gmail.com>
2021-10-08 21:49:33 +0100

    my first komit

---
+++ lib/Input_Page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF090C21),
+        title: const Center(child: Text('BMI CALCULATOR')),
+      ),
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'Input_Page.dart';
+
+void main() {
+  runApp(const BMICalculator(),);
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0D22),
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+        // textTheme: TextTheme(//to set all the text color to white
+         // bodyText1: TextStyle(color: Colors.white),
+    ),
+      home: const InputPage(),
+
+    );
+  }
+}
+
