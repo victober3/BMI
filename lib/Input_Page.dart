d002f8a12315835d66eaaedb028e081c131f6401
victober3 <vicobabolodje@gmail.com>
2021-10-13 06:57:00 +0100

    new beginig

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+ const BottomContainerHeight = 80.0;
+ const activeCardColor = Color(0xFF1D1F33) ;
+ const bottomContainerColor =  Color(0xffEB1555);
+
 class Input_Page extends StatefulWidget {
   //const InputPage({Key? key}) : super(key: key);
 
@@ -12,7 +16,7 @@ class _Input_PageState extends State<Input_Page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF090C21),
+        backgroundColor: activeCardColor,
         title: const Center(
           child: Text('BMI CALCULATOR'),
         ),
@@ -23,28 +27,53 @@ class _Input_PageState extends State<Input_Page> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Reuseable(),
+                  child: Reuseable(
+                    colour: activeCardColor,
+                  ),
+
                 ),
                 Expanded(
-                  child: Reuseable()
+                  child: Reuseable(
+                    colour: activeCardColor,
+                  )
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Reuseable()
+            child: Reuseable(
+              colour: activeCardColor,
+            )
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Reuseable()
+                  child: Reuseable(
+                    colour: activeCardColor,
+                  )
                 ),
                 Expanded(
-                  child: Reuseable()
+                  child: Reuseable(
+                    colour: activeCardColor,
+                  )
                 ),
               ],
             ),
+          ),
+          Container(
+            child: Center(
+              child: Text(
+                'CALCULATE YOUR BMI',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+              ),
+            ),
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: BottomContainerHeight,
           ),
         ],
       ),
@@ -53,16 +82,15 @@ class _Input_PageState extends State<Input_Page> {
 }
 
 class Reuseable extends StatelessWidget {
-  const Reuseable({
-    Key? key,
-  }) : super(key: key);
+  Reuseable({required this.colour});
+  Color colour;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,//Color(0xFF1D1E33),
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
